8734f1270afe5a42a88da0e24512949874caa740
ergeakova <ergeak@gmail.com>
2022-08-24 15:20:54 +0300

    added UIdesing and UItest

--- SwiftStoryboardUITestExample/ViewController.swift
+++ SwiftStoryboardUITestExample/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var todoList = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
     }
-
-
+    
+    @IBAction func addButtonClicked(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Item", message: "Enter your new TO DO", preferredStyle: .alert)
+        alert.addTextField{ textField in
+            textField.placeholder = "Enter Item"
+        }
+        
+        let okButton = UIAlertAction(title: "OK", style: .default) { action in
+            guard let textField = alert.textFields?[0], let inputText = textField.text, !inputText.isEmpty else{
+                return
+            }
+            
+            self.todoList.insert(inputText, at: 0)
+            self.tableView.insertRows(at: [.init(row: 0, section: 0)], with: .automatic)
+        }
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = todoList[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.todoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
-
--- SwiftStoryboardUITestExampleUITests/SwiftStoryboardUITestExampleUITests.swift
+++ SwiftStoryboardUITestExampleUITests/SwiftStoryboardUITestExampleUITests.swift
@@ -8,34 +8,44 @@
 import XCTest
 
 class SwiftStoryboardUITestExampleUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testToDoItem() throws{
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
+        let addButton = app.navigationBars["SwiftStoryboardUITestExample.View"].buttons["Add"]
+        let textField =  app.alerts["Add Item"].scrollViews.otherElements.collectionViews.textFields["Enter Item"]
+        let okBUtton = app.alerts["Add Item"].scrollViews.otherElements.buttons["OK"]
+        let addedCell = app.tables.cells.containing(.staticText, identifier:"my to do").element
+        
+        addButton.tap()
+        textField.tap()
+        textField.typeText("my to do")
+        okBUtton.tap()
+        
+        XCTAssertTrue(addedCell.exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testDeleteItem() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let addButton = app.navigationBars["SwiftStoryboardUITestExample.View"].buttons["Add"]
+        let textField =  app.alerts["Add Item"].scrollViews.otherElements.collectionViews.textFields["Enter Item"]
+        let okBUtton = app.alerts["Add Item"].scrollViews.otherElements.buttons["OK"]
+        let addedCell = app.tables.cells.containing(.staticText, identifier:"my to do").element
+        let tablesQuery = app.tables
+        let deleteButton = tablesQuery.buttons["Delete"]
+        
+        addButton.tap()
+        textField.tap()
+        textField.typeText("my to do")
+        okBUtton.tap()
+        
+        addedCell.swipeLeft()
+        deleteButton.tap()
+        
+        XCTAssertFalse(addedCell.exists)
     }
 }
