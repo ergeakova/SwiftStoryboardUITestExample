dfad76d89e8c119005e0fca274ef09d2c79e3c18
ergeakova <ergeak@gmail.com>
2022-08-24 14:24:44 +0300

    Initial Commit

---
+++ SwiftStoryboardUITestExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftStoryboardUITestExample
+//
+//  Created by Erge Gevher Akova on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
